41d263cd5ea086154a1a19512e60b0c767115d48
Ritisha Bajaj <ritisha2503@gmail.com>
2026-06-13 11:29:41 +0530

    writing data_loader.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from src.data_loader import load_all_documents
+# from src.vectorstore import FaissVectorStore
+# from src.search import RAGSearch
+
+# Example usage 
+if __name__ == "__main__":
+    docs = load_all_documents("data")

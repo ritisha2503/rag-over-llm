ad7d87ec78b424659a866074f59690719f7ed470
Ritisha Bajaj <ritisha2503@gmail.com>
2026-06-13 11:40:50 +0530

    writing embedding.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from src.data_loader import load_all_documents
+from src.embedding import EmbeddingPipeline
 # from src.vectorstore import FaissVectorStore
 # from src.search import RAGSearch
 
 # Example usage 
 if __name__ == "__main__":
     docs = load_all_documents("data")
+    chunks = EmbeddingPipeline().chunk_documents(docs)
+    chunkvectors = EmbeddingPipeline().embed_chunks(chunks)
+    print(chunkvectors)

1dd3c20da9f824a88358a47858b21b9864fd734c
Ritisha Bajaj <ritisha2503@gmail.com>
2026-06-13 16:18:59 +0530

    writing vectorstore.py and search.py and adding .env to .gitignore for api key

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from src.data_loader import load_all_documents
 from src.embedding import EmbeddingPipeline
-# from src.vectorstore import FaissVectorStore
-# from src.search import RAGSearch
+from src.vectorstore import FaissVectorStore
+from src.search import RAGSearch
 
-# Example usage 
 if __name__ == "__main__":
     docs = load_all_documents("data")
-    chunks = EmbeddingPipeline().chunk_documents(docs)
-    chunkvectors = EmbeddingPipeline().embed_chunks(chunks)
-    print(chunkvectors)
+    store = FaissVectorStore("faiss_store")
+    #store.build_from_documents(docs)
+    store.load()
+    #print(store.query("What is attention mechanism?", top_k=3))
+    rag_search = RAGSearch()
+    query = "What is attention mechanism?"
+    summary = rag_search.search_and_summarize(query, top_k=3)
+    print("Summary:", summary)
